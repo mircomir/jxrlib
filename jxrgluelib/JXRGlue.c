458f3b2cf69a77dba7380c534db70fc52b30bed0
Mirco Miranda <mirco.miranda@systemceramics.com>
2026-05-26 09:34:22 +0200

    Cleaning up unnecessary code

--- jxrgluelib/JXRGlue.c
+++ jxrgluelib/JXRGlue.c
@@ -465,11 +465,6 @@ ERR PKCodecFactory_CreateDecoderFromFile(const char* szFilename, PKImageDecode**
     pDecoder->fStreamOwner = !0;
 
 Cleanup:
-#if 0 // disabled due to possible wrong usage of the library in the past
-    if(pStream && pDecoder && !pDecoder->fStreamOwner) {
-        CloseWS_File(&pStream); // MMIR: Fix memory leak on error
-    }
-#endif
     return err;
 }
 
