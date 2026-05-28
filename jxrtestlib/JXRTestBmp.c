8807b18d685e0c5347017362ab4ab29b6b53a3e4
Mirco Miranda <mirco.miranda@systemceramics.com>
2026-05-28 10:09:45 +0200

    Fix issues reported by github

--- jxrtestlib/JXRTestBmp.c
+++ jxrtestlib/JXRTestBmp.c
@@ -211,7 +211,7 @@ ERR PKImageEncode_WritePixels_BMP(
 
     for (i = cLine - 1; 0 <= i; --i)
     {
-        size_t offM = cbStride * i;
+        size_t offM = (size_t)cbStride * i;
         size_t offS = (size_t)cbLineS * (pIE->uHeight - (pIE->idxCurrentLine + i + 1));
 
         Call(pS->SetPos(pS, pIE->offPixel + offS));
